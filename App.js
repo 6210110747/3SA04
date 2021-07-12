4a40bffc2134f6c620b42806a4bdc50d2468cec8
6210110747 <6210110747@psu.ac.th>
2021-07-12 10:00:59 +0700

    1

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App() { 
+  const doIt = () => {     
+    console.log("Hello from console")  
+   } 
+
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
