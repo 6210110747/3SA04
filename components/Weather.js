6af78bcfad727e6c6df947962913179fbc5afd82
6210110747 <6210110747@psu.ac.th>
2021-07-17 22:59:35 +0700

    Part 4

--- components/Weather.js
+++ components/Weather.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect,useState } from 'react'
 import { ImageBackground, StyleSheet, Text } from 'react-native'
 import Forecast from './Forecast'
   
 export default function Weather(props) {
-    const [forecastInfo, setForecastInfo] = useState ({          
+     const [forecastInfo, setForecastInfo] = useState ({          
         main: '-',         
         description: '-',         
         temp: 0     
-    })        
+    })  
+    useEffect(() => {         
+        console.log(`fetching data with zipCode = ${props.zipCode}`)         
+        if (props.zipCode) {             
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=6daa99ea35353a3f64bccc7a192a05c8`)                 
+            .then((response) => response.json())                 
+            .then((json) => {                     
+                setForecastInfo({                         
+                    main: json.weather[0].main,                         
+                    description: json.weather[0].description,                         
+                    temp: json.main.temp                     
+                });                 
+            })                 
+            .catch((error) => {                     
+                console.warn(error);                 
+            });         
+        } }, [props.zipCode])
+
+         
         return (   
             <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
                 <Text>Zip Code</Text>
@@ -20,7 +38,10 @@ export default function Weather(props) {
 
 const styles = StyleSheet.create({
     backdrop:{
+        flexDirection: 'column',          
         alignItems: 'center',
+        //backgroundColor: 'rgba(52, 52, 52, 0.8)',
+        //fontWeight: 'bold', 
         width: '100%',
         height: '100%'
     }
